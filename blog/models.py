da624cd972fc4c4cb30b8129a9f1136f8ccd3c16
mustapha adubiagbe <mustapha adubiagbe>
2017-02-22 16:10:41 +0100

    last commit

--- blog/models.py
+++ blog/models.py
@@ -2,19 +2,16 @@ from __future__ import unicode_literals
 
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class Post(models.Model):
-    author = models.ForeignKey('auth.User')
-    title = models.CharField(max_length=200)
-    text = models.TextField()
-    created_date = models.DateTimeField(default=timezone.now)
-    published_date = models.DateTimeField(blank=True, null=True)
-
-    def publish(self):
-        self.published_date = timezone.now()
-        self.save()
+    author = models.ForeignKey(User)
+    title = models.CharField(blank=False, max_length=200)
+    text = models.TextField(blank=False)
+    created_date = models.DateTimeField(auto_now_add=True)
+    published_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
